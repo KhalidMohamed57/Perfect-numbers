6bc75464fa46a7ff127d8ad062549230d9044ad5
KhalidMohamed57 <121644086+KhalidMohamed57@users.noreply.github.com>
2022-12-30 16:14:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER 1St NUMBER");
+            int a = int.Parse(Console.ReadLine()!);
+
+
+            Console.WriteLine("ENTER 2Scd NUMBER");
+
+            int s = int.Parse(Console.ReadLine()!);
+
+            for (int j = a; j <= s; j++)
+            {
+               int i = 1;
+             int   sum = 0;
+                while (i < j)
+                {
+                    if (j % i == 0)
+                        sum += i;
+                    i++;
+                }
+                if (sum ==j)
+                    Console.WriteLine(j+"is perfect number");
+            }
+        }
+    }
+}
